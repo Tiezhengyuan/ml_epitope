139fced9bf7b2f36dd7cd1d3f60772759333f0ea
Tiezheng Yuan <tiezhengyuan@hotmail.com>
2025-04-01 10:57:37 -0400

    AA composition

--- prepare_datasets/aa_comp.py
+++ prepare_datasets/aa_comp.py
@@ -16,59 +16,56 @@ class AAComp:
 
     @staticmethod
     def isolate_epitope_seq(data_iter):
-
         aa_data = {}
         num_pro, num_epi = 0, 0
-        for data, path in data_iter:
-            print(path)
-            for acc in data:
-                pro_seq = data[acc]['pro_seq']
-                c = IsolateAA(data[acc])
+        for acc, rec in data_iter:
+            if 'pro_seq' in rec:
+                pro_seq = rec['pro_seq']
+                c = IsolateAA(rec)
                 aa_data[acc] = {
                     'epitope_seq': c.get_epi_seq(),
                     'other_seq': c.get_other_seq(),
                 }
                 num_pro += 1
-                num_epi += len(data[acc]['epitopes'])
-                # if len((aa_data[acc]['other_seq']) == len(pro_seq):
-                #     print(acc)
-                #     print(data[acc]['epitopes'])
-                #     print(aa_data[acc])
+                num_epi += len(rec['epitopes'])
         print(f"Number of proteins = {num_pro}")
         print(f"Number of epitopes = {num_epi}")
         return aa_data, num_pro, num_epi
 
     @staticmethod
     def retrieve_epitope_2aa(data_iter):
+        '''
+        epi_counts: count 2-AA in epitopes,
+        total_epi: number of epitopes,
+        other_counts: count 2-AA of non-epitopes, 
+        total_other: number of non-epitopes
+        '''
         total_epi, total_other = 0, 0
         epi_counts, other_counts = Counter(), Counter()
-        for data, path in data_iter:
-            print(path)
-            for acc in data:
-                c = IsolateAA(data[acc])
-                epi_len = len(c.get_epi_seq())
-                other_len = len(c.get_other_seq())
-                epi_seq, other_seq = c.isolate_2aa()
-                epi_counts += Counter(epi_seq)
-                total_epi += epi_len
-                other_counts += Counter(other_seq)
-                total_other += other_len
+        for acc, rec in data_iter:
+            c = IsolateAA(rec)
+            epi_len = len(c.get_epi_seq())
+            other_len = len(c.get_other_seq())
+            epi_seq, other_seq = c.isolate_2aa()
+            epi_counts += Counter(epi_seq)
+            total_epi += epi_len
+            other_counts += Counter(other_seq)
+            total_other += other_len
         return epi_counts, total_epi, other_counts, total_other
 
     @staticmethod
     def retrieve_frequency(data_iter, k:int):
         num_epi = 0
         epi_counts = {}
-        for data, path in data_iter:
-            for acc in data:
-                c = IsolateAA(data[acc])
-                aa_counts = c.kmer_counts(k)
-                num_epi += c.num_epitopes()
-                for aa, count in aa_counts.items():
-                    if aa not in epi_counts:
-                        epi_counts[aa] = count
-                    else:
-                        epi_counts[aa] += count
+        for acc, rec in data_iter:
+            c = IsolateAA(rec)
+            aa_counts = c.kmer_counts(k)
+            num_epi += c.num_epitopes()
+            for aa, count in aa_counts.items():
+                if aa not in epi_counts:
+                    epi_counts[aa] = count
+                else:
+                    epi_counts[aa] += count
         aa_freq = pd.DataFrame({
             'aa': epi_counts.keys(),
             'counts': epi_counts.values(),
